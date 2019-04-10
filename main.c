33afe009f8e085389c603a9964c10884e5b38eca
AlexanderGonzo <alexgonzo1994@gmail.com>
2019-04-10 12:38:36 -0500

    Need to add index print to sorted array and fix output format.

--- main.c
+++ main.c
@@ -6,19 +6,7 @@
 #define MAX_NUM 56
 
 
-
-void printArr(int *a, int len) {
-    for (int i = 1; i < len/3; i++) {
-        printf("%d ", i);
-    }
-    printf("\n");
-    for (int i = 1; i < len/3; i++)
-        printf("%d ", a[i]);
-    printf("\n");
-
-}
-
-void sortmyArray(int arr[]){
+void sortmyArray(int *arr){
     int temp,i,j = 0;
 
     for(i=0;i< MAX_NUM;i++)
@@ -33,36 +21,83 @@ void sortmyArray(int arr[]){
             }
         }
     }
-    printArr(arr,MAX_NUM);
+    //printSortedArr(arr,MAX_NUM);
 }
-void menu(){
+void printArr(int *a, int len) {
+    for (int i = 1; i < 11; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    for (int i = 1; i < 11; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+
+}
+void printSortedArr(int *a, int len) {
+    for (int i = 0; i < 11; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+
+}
+void findIndex(int *a, int *b) {
+    int i;
+    int tempSortedArray[MAX_NUM] = {0};
+
+    for(i = 0; i < MAX_NUM ; i++){
+
+        if(b[i] == a[i]){
+            tempSortedArray[i-1] = i;
+
+            //printf("%d ", i);
+        }
+    }
+    sortmyArray(tempSortedArray);
+    printSortedArr(tempSortedArray,MAX_NUM);
+}
+
+void printFirst(int *a, int len){
+    printf("\t ");
+    for (int i = 1; i < 11; i++)
+        printf("%d\t", a[i]);
+    printf("\n");
+}
+
+
+
+int main() {
+
+    FILE *fp;
+    fp = freopen("lottofile.txt", "r", stdin);
+
     printf("This Program is Written by Alexander and Andrew Gonzalez\n");
     printf("The function of this program is to find the frequency\n"
            "of each number that has been drawn in the Texas lotto game.\n"
            "The program :\n");
     printf("   1. Calculates and displays the frequency of drawn numbers\n"
-           "       in Texas Lotto in each year.");
+           "       in Texas Lotto in each year.\n");
     printf("   2. Calculates and displays the overall frequency of each\n"
-           "       number that has been drawn since the lotto started.");
+           "       number that has been drawn since the lotto started.\n");
     printf("   3. Sorts the frequency numbers in Descending order. Display\n"
-           "      the sorted list.");
+           "      the sorted list.\n");
     printf("   4. Display total number of integers in the entire file. \n");
-}
-
-int main() {
-    FILE *fp;
-    fp = freopen("lottofile.txt", "r", stdin);
-
+    printf("\n");
 
     int currentarr[MAX_NUM + 1]= {0};
     int overallarr[MAX_NUM + 1]= {0};
+    int tempArr[MAX_NUM + 1] = {0};
     int year;
     int temp;
     int digitCOUNTER=0;
     int tempYear = -1;
-    //printf("%s","Year ->  No\t 1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n");
+
+    printf("%s", "For each year, the frequency of drawn numbers in the Texas\n");
+    printf("%s", "Lotto are as follows :- \n");
+    printf("\n");
+    printf("%s","Year ->  No\t 1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n");
+    printf("\n");
     printf("|");
     printf("\n");
+
     while (!feof(fp)){
 
 
@@ -76,7 +111,8 @@ int main() {
         }else if (year != tempYear ){
             printf("%d\t", tempYear);
 
-            printArr(currentarr,MAX_NUM);
+            printFirst(currentarr,MAX_NUM);
+            printf("\n");
             memset(currentarr, 0, sizeof(currentarr));
 
         }
@@ -84,7 +120,6 @@ int main() {
         for (int i = 0; i < NUMS_TO_READ; i++) {
             scanf("%d", &temp);
             digitCOUNTER++;
-            //printf("%d ", temp);
             currentarr[temp]++;
             overallarr[temp]++;
         }
@@ -95,24 +130,34 @@ int main() {
 
 
     printf("%d\t", tempYear);
-    printArr(currentarr, MAX_NUM);
+    printFirst(currentarr, MAX_NUM);
     printf("\n");
     printf("Over All The Data , The Frequency of drawn numbers in the \n");
-    printf("Texas Lotto are     :-\n");
-   // printf("%s","No -->     \t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n");
-    printf("|\n");
-    printf("Frequency");
+    printf("Texas Lotto are :-\n");
+    printf("%s", "No --->");
+    printf("%s", "|");
+    printf("\n");
+
+    printf("Frequency: ");
     printArr(overallarr,MAX_NUM);
     printf("\n");
 
     printf("The MOST frequent Numbers Drawn in the Texas LOTTO Game from \n");
     printf("Highest to Lowest \n");
-    sortmyArray(overallarr);
+
+
+    //Copy overall array into a temp array
+    //Sort overall, Then find the number in the temp array returning the index.
+    memcpy(tempArr, overallarr, sizeof(overallarr));
+    sortmyArray(tempArr);
+
+    findIndex(overallarr,tempArr);
+    printSortedArr(tempArr,MAX_NUM);
+
     printf("\n");
     printf("%s %d", "Total number of integers read : ", digitCOUNTER);
     printf("\n");
 
-    //printArr(currentarr, MAX_NUM);
     return 0;
 
 }
